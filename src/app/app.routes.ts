0b789456df32fb8a37cd7ff96a48d33fa2e1b48b
mohammad karimi <jaavdanm@gmail.com>
2024-10-31 22:58:55 +0330

    Add today tasks page and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
 	{
+		path: "",
+		redirectTo: "today-tasks",
+		pathMatch: "full"
+	},
+	{
 		path: "all-tasks",
 		loadComponent: () => import('./pages/all-tasks/all-tasks.component').then(c => c.AllTasksComponent)
 	},
 	{
+		path: "completed-tasks",
+		loadComponent: () => import('./pages/completed-tasks/completed-tasks.component').then(c => c.CompletedTasksComponent)
+	},
+	{
+		path: "today-tasks",
+		loadComponent: () => import('./pages/today-tasks/today-tasks.component').then(c => c.TodayTasksComponent)
+	},
+	{
 		path: "**",
-		redirectTo: "daily-tasks"
+		redirectTo: "today-tasks"
 	}
 ];

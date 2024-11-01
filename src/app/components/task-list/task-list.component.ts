93a47d5ccb96d6550c0a00ff106feb92b3baa251
mohammad karimi <jaavdanm@gmail.com>
2024-11-01 19:02:10 +0330

    Task item and task list components

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { TaskItemComponent } from '@components/task-item/task-item.component';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [
+	MatButtonModule,
+	MatIconModule,
+	TaskItemComponent
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })

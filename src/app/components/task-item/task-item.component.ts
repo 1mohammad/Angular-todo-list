64bd317b84704d6b8ce773d855febfa58a04a15f
Mohammad Karimi <mohammad.k.dev@gmail.com>
2024-11-01 19:03:40 +0330

    Task item and task list components (#11)

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [
+	MatCardModule,
+	MatButtonModule,
+	MatIconModule,
+	MatCheckboxModule,
+	MatMenuModule
+  ],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
--- src/app/pages/today-tasks/today-tasks.component.ts
+++ src/app/pages/today-tasks/today-tasks.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { TaskListComponent } from '@components/task-list/task-list.component';
 
 @Component({
   selector: 'app-today-tasks',
   standalone: true,
-  imports: [],
+  imports: [
+	TaskListComponent
+  ],
   templateUrl: './today-tasks.component.html',
   styleUrl: './today-tasks.component.scss'
 })
